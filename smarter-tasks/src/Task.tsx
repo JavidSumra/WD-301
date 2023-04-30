8fcd6685d0d3988005172f3105450c4ad9aaac78
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-04-30 18:52:06 +0530

    Function Base Component

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -5,23 +5,30 @@ interface TaskProp {
   title: string;
   dueDate: string;
   description: string;
+  removeTask: (title: string) => void;
 }
 
-class Task extends React.Component<TaskProp> {
-  render() {
-    return (
-      <div className="TaskItem">
-        <hr />
+const Task = (props: TaskProp) => {
+  return (
+    <div className="TaskItem">
+      <hr />
+      <li>
         <div className="shadow-md border border-slate-100 p-2 m-2">
-          <h2 className="text-base font-bold my-1">{this.props.title}</h2>
-          <p className="text-sm text-slate-500">{this.props.dueDate}</p>
+          <h2 className="text-base font-bold my-1">{props.title}</h2>
+          <p className="text-sm text-slate-500">{props.dueDate}</p>
           <p className="text-sm text-slate-500">
-            Description:{this.props.description}
+            Description:{props.description}
           </p>
+          <button
+            className="deleteTaskButton bg-red-400 p-1 rounded"
+            onClick={() => props.removeTask(props.title)}
+          >
+            Delete Task
+          </button>
         </div>
-      </div>
-    );
-  }
-}
+      </li>
+    </div>
+  );
+};
 
 export default Task;
