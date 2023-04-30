922cb61fb9a07ed3a91b779324ac8a3da4dbf3e6
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-04-30 18:53:01 +0530

    Function Base Component

--- smarter-tasks/src/TaskApp.tsx
+++ smarter-tasks/src/TaskApp.tsx
@@ -1,42 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TaskItem } from "./types";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 interface TaskAppProp {}
 interface TaskAppState {
   tasks: TaskItem[];
 }
-class TaskApp extends React.Component<TaskAppProp, TaskAppState> {
-  constructor(props: TaskAppProp) {
-    super(props);
-    this.state = {
+
+const TaskApp = (props: TaskAppProp) => {
+  const [taskAppState, setTaskAppState] = useLocalStorage<TaskAppState>(
+    "tasks",
+    {
       tasks: [],
-    };
-  }
-  addTask = (task: TaskItem) => {
-    this.setState((state, props) => {
-      return {
-        tasks: [...state.tasks, task],
-      };
+    }
+  );
+  const addTask = (task: TaskItem) => {
+    setTaskAppState({ tasks: [...taskAppState.tasks, task] });
+  };
+  const removeTask = (title: string) => {
+    setTaskAppState({
+      tasks: taskAppState.tasks.filter((item) => item.title !== title),
     });
   };
-  render() {
-    return (
-      <div className="m-5 mx-40 font-medium">
-        <h1 className="text-3xl">Smarter Tasks</h1>
-        <p className="text-small my-1">
-          Project: Graduation Final Year Project(Revamp College Website)
-        </p>
-        <div className="flex justify-evenly items-center font-medium my-10">
-          <div className="border border-slate-700 rounded p-7 w-96 h-max">
-            <TaskForm addTask={this.addTask} />
-            <TaskList tasks={this.state.tasks} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <TaskForm addTask={addTask} />
+      <TaskList tasks={taskAppState.tasks} removeTask={removeTask} />
+    </div>
+  );
+};
 
 export default TaskApp;
