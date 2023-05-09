f82e639e46bc1c46877e71643f0319bb7413ead6
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-05-09 12:49:45 +0530

    Adding Not Found Page

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./TaskCard.css";
 
 interface TaskProp {
   title: string;
   dueDate: string;
   description: string;
+  id: string;
   removeTask: (title: string) => void;
 }
 
@@ -14,7 +16,9 @@ const Task = (props: TaskProp) => {
       <hr />
       <li>
         <div className="shadow-md border border-slate-100 p-2 m-2">
-          <h2 className="text-base font-bold my-1">{props.title}</h2>
+          <Link to={`/tasks/${props.id}`}>
+            <h2 className="text-base font-bold my-1">{props.title}</h2>
+          </Link>
           <p className="text-sm text-slate-500">{props.dueDate}</p>
           <p className="text-sm text-slate-500">
             Description:{props.description}
