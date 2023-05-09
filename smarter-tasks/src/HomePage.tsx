762d2fbf4977c48802adef4467f3681b443e7071
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-05-09 12:46:39 +0530

    Adding Not Found Page

---
+++ smarter-tasks/src/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center text-3xl h-screen flex-col">
+      <h1 className="p-3">Task Manager</h1>
+      <p>Welcome to the Task Manager application</p>
+    </div>
+  );
+};
+
+export default HomePage;
