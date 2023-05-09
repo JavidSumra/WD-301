d2c7393c92432419e723e97ce6b1eb7eff4ef8b7
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-05-09 12:49:00 +0530

    Adding Not Found Page

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,10 +1,38 @@
+import React from "react";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import "./App.css";
+
+import Header from "./Header";
+import HomePage from "./HomePage";
 import TaskApp from "./TaskApp";
-import "./TaskCard.css";
+import TaskDetailsPage from "./TaskDetailsPage";
+import Signin from "./Signin";
+import NotFound from "./NotFound";
+import { ProtectedRoute } from "./ProtectedRoute";
+
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <TaskApp />
-    </div>
+    <>
+      {location.pathname !== "/signin" && location.pathname !== "/notfound" && (
+        <Header />
+      )}
+      <Routes>
+        <Route path="/" element={<ProtectedRoute element={<HomePage />} />} />
+        <Route
+          path="/tasks"
+          element={<ProtectedRoute element={<TaskApp />} />}
+        />
+        <Route
+          path="/tasks/:id"
+          element={<ProtectedRoute element={<TaskDetailsPage />} />}
+        />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="*" element={<Navigate to={"/notfound"} />} />
+      </Routes>
+    </>
   );
 }
 
