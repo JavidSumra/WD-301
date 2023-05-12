41b253dc9f54a355f2ffbceb92f2ca144976804c
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-05-12 07:46:15 +0530

    Changing Home Page

--- smarter-tasks/src/HomePage.tsx
+++ smarter-tasks/src/HomePage.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center text-3xl h-screen flex-col">
-      <h1 className="p-3">Task Manager</h1>
-      <p>Welcome to the Task Manager application</p>
-    </div>
+    <h1 className="flex items-center justify-center text-3xl h-screen flex-col">
+      Welcome to Task Manager
+    </h1>
   );
 };
 
