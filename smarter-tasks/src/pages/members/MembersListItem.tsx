22a572d452a5a6a6641fd13e900d1e2c08d14884
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-06-23 14:48:06 +0530

    Update MembersListItem.tsx

--- smarter-tasks/src/pages/members/MembersListItem.tsx
+++ smarter-tasks/src/pages/members/MembersListItem.tsx
@@ -31,11 +31,13 @@ export default function MembersListItem() {
           className="member flex flex-col flex-wrap p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <div className="flex items-center justify-end">
+            <button onClick={() => handleDelete(member.id)}>
             <TrashIcon
               className="h-6 w-6 hover:text-red-500 duration-75"
               aria-hidden="true"
-              onClick={() => handleDelete(member.id)}
+         
             ></TrashIcon>
+            </button>
           </div>
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white ">
             Name:{member.name}
