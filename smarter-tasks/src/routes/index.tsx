f5bdb1c9d161fd78ead3e7b2f5f5078d4a3518f5
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-06-23 11:32:10 +0530

    Adding Members Page

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -7,6 +7,7 @@ import Signup from "../pages/signup";
 import Projects from "../pages/projects";
 import Members from "../pages/members";
 import Logout from "../pages/logout";
+import NotFound from "../NotFound";
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/account/projects" replace /> },
@@ -44,6 +45,10 @@ const router = createBrowserRouter([
         element: <Members />,
       },
     ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
