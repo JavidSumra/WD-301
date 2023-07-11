e895f0cb90c36d58c8f1537ae71980a479a1ff18
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-07-11 21:59:37 +0530

    Adding Lvl 8 Content

--- smarter-tasks/src/pages/projects/ProjectListItems.tsx
+++ smarter-tasks/src/pages/projects/ProjectListItems.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { useProjectsState } from "../../context/projects/context";
 export default function ProjectListItems() {
@@ -16,14 +17,15 @@ export default function ProjectListItems() {
   return (
     <>
       {projects.map((project: any) => (
-        <div
+        <Link
           key={project.id}
+          to={`${project.id}`}
           className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
             {project.name}
           </h5>
-        </div>
+        </Link>
       ))}
     </>
   );
