fd154c9903638efd42c6eec7e0602e5049733438
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-07-11 21:58:21 +0530

    Adding Lvl 8 Content

--- smarter-tasks/src/pages/projects/ProjectList.tsx
+++ smarter-tasks/src/pages/projects/ProjectList.tsx
@@ -1,14 +1,8 @@
-import React, { useEffect } from "react";
-import { fetchProjects } from "../../context/projects/actions";
+import React from "react";
 
-import { useProjectsDispatch } from "../../context/projects/context";
 import ProjectListItems from "./ProjectListItems";
 
 const ProjectList: React.FC = () => {
-  const dispatchProjects = useProjectsDispatch();
-  useEffect(() => {
-    fetchProjects(dispatchProjects);
-  }, []);
   return (
     <div className="grid gap-4 grid-cols-4 mt-5">
       <ProjectListItems />
