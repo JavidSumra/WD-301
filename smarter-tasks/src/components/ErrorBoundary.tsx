f4b6240f38f16d1d2324d2e8ce50e3db32352bff
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-07-22 12:11:11 +0530

    Update ErrorBoundary.tsx

--- smarter-tasks/src/components/ErrorBoundary.tsx
+++ smarter-tasks/src/components/ErrorBoundary.tsx
@@ -3,13 +3,15 @@ import React, { Component, ErrorInfo, ReactNode } from "react";
 interface ErrorBoundaryState {
   hasError: boolean;
 }
-
-class ErrorBoundary extends Component<{}, ErrorBoundaryState> {
+interface Props {
+  children?: ReactNode;
+}
+class ErrorBoundary extends Component<Props, ErrorBoundaryState> {
   constructor(props: {}) {
     super(props);
     this.state = { hasError: false };
   }
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     return { hasError: true };
   }
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
